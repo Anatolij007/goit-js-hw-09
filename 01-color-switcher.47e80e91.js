649b0b2f8067076ec9063e05c24e020461575e27
Anatolij007 <98270281+Anatolij007@users.noreply.github.com>
2022-08-17 20:02:15 +0000

    Deploying to gh-pages from @ Anatolij007/goit-js-hw-09@ba2df894d186073ead6210b8db09e8e1471acd2b 🚀

---
+++ 01-color-switcher.47e80e91.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.start.addEventListener("click",(function(){e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.disabled=!1,t.stop.disaled=!0}));var e=null;t.stop.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.47e80e91.js.map
